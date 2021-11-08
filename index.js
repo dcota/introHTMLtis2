a8888ca8d91206ab14a62ca886601d4b0a783e3f
duCota <duartecota@enta.pt>
2021-11-08 12:41:02 -0100

    aula 7nov2021

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { prototype } = require('stream')
  
 app.use(express.static('./public'))
 
@@ -8,5 +9,9 @@ app.get('/', function (req, response) {
   response.sendFile(path.join(_dirname, '/public/index.html'))
 })
 
+const port = 5000;
 
-app.listen(3000) 
+app.listen(port, function (){
+  console.log("Listenning on port: " + port)
+})
+
